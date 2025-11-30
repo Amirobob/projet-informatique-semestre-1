47f1bc7f852b203258b14d6b25ed06a0d2dba28f
Amir <Amirobob10@gmail.com>
2025-11-30 12:52:12 +0100

    updates

--- Game/main.c
+++ Game/main.c
@@ -8,9 +8,8 @@
 #include <string.h>
 #include <conio.h>
 #include <stdbool.h>
-// Map[x][y] where x is column, y is row
+
 char shapepick(char map[xmax][ymax], int x, int y) {
-    // Try shapes that won't create a new triple with neighbors
     char candidates[3] = { CIRCLE, SQUARE, TRIANGLE };
     for (int k = 0; k < 3; k++) {
         char s = candidates[(k + rand()) % 3];
@@ -40,6 +39,7 @@ char shapepick(char map[xmax][ymax], int x, int y) {
         default:       return CIRCLE;
     }
 }
+
 void generate_map(char map[xmax][ymax]) {
     int temp;
     for (int y = 0; y < ymax; y++) {
@@ -121,9 +121,34 @@ void stat(int y, int level, int life, int score, int timeremaining, int turnslef
     }
 }
 
-void print_map(char map[xmax][ymax], int level, int life, int score, int timeremaining, int turnsleft, int squareleft, int triangleleft, int circleleft) {
+void menu(void) {
+    set_color(WHITE, BLACK);
+    char menu[9][32] = {
+        "|-----------------------------|",
+        "|         ECE Heroes!         |",
+        "|-----------------------------|",
+        "|       1. Start Game         |",
+        "|       2. Continue Game      |",
+        "|       3. Instructions       |",
+        "|       4. Exit               |",
+        "|-----------------------------|"
+    };
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j<32; j++) {
+        set_color(WHITE, BLACK);
+        if (i == 1 && j>2 && j<30) set_color(YELLOW, BLACK);
+        else if (i == 3 && j>2 && j<30 || i == 4 && j>2 && j<30) set_color(GREEN, BLACK);
+        else if (i == 5 && j>2 && j<30) set_color(BLUE, BLACK);
+        else if (i == 6 && j>2 && j<30) set_color(RED, BLACK);
+        else set_color(WHITE, BLACK);
+        printf("%c", menu[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+bool print_map(char map[xmax][ymax], int level, int life, int score, int timeremaining, int turnsleft, int squareleft, int triangleleft, int circleleft) {
     // Top coordinate header
-    SetConsoleOutputCP(65001);
     set_color(WHITE, BLACK);
     printf("   ");
     for (int x = 0; x < xmax; x++);
@@ -150,6 +175,7 @@ void print_map(char map[xmax][ymax], int level, int life, int score, int timerem
         printf("\n");
     }
     set_color(WHITE, BLACK);
+    return true;
 }
 
 void print_stats(int level, int life, int score, int timeremaining, int turnsleft, int squareleft, int triangleleft, int circleleft) {
@@ -167,25 +193,82 @@ void print_stats(int level, int life, int score, int timeremaining, int turnslef
     printf("Circles left: %d\n", circleleft);
 }
 
-void legend(void) {
+void saveprogress(int level) {
+    char playername[50];
     set_color(WHITE, BLACK);
-    printf("\nLegend: ");
-    set_color(BLUE, BLACK);   printf("%c Circle  ", CIRCLE);
-    set_color(YELLOW, BLACK); printf("%c Square  ", SQUARE);
-    set_color(GREEN, BLACK);  printf("%c Triangle  ", TRIANGLE);
-    set_color(MAGENTA, BLACK);printf("# Border\n\n");
+    printf("Enter your name to save progress: ");
+    scanf("%49s", playername);
+}
+
+void endscreen(bool gameover, int level) {
+    if (gameover) {
+        char gameovermessage[][18] = {
+            " _______________ ",
+            "|  ___________  |",
+            "| | game over | |",
+            "| |___________| |",
+            "|_______________|",
+        };
+        set_color(RED, BLACK);
+        for (int i = 0; i < 5; i++) {
+            printf("%s\n", gameovermessage[i]);
+        }
+        set_color(WHITE, BLACK);
+        printf("Better luck next time!\n");
+        printf(" ________________\n");
+        printf("|Try again       |\n");
+        printf("|________________|\n");
+        printf("|save last level |\n");
+        printf("|________________|\n");
+        printf("|leave game      |\n");
+        printf("|________________|\n");
+    } else {
+        char won[][34] = {
+            " _______________________________ ",
+            "|  ___________________________  |",
+            "| | congratulations, you won! | |",
+            "| |___________________________| |",
+            "|_______________________________|",
+        };
+        set_color(GREEN, BLACK);
+        for (int i = 0; i < 5; i++) {
+            printf("%s\n", won[i]);
+        }
+        set_color(WHITE, BLACK);
+        printf("       ________________\n");
+        printf("      |next level      |\n");
+        printf("      |________________|\n");
+        printf("      |save progress   |\n");
+        printf("      |________________|\n");
+        printf("      |leave game      |\n");
+        printf("      |________________|\n");
+        saveprogress(level);
+    }
+}
+
+void leveload(void) {
+    char playername[50];
     set_color(WHITE, BLACK);
+    printf("Enter your name to load progress: ");
+    scanf("%49s", playername);
+    printf("welcome back john doe! loading level 2...\n");
+    printf("\n name not found, try again\n");
 }
 
 int main() {
+    clrscr();
+    show_cursor();
+    gotoxy(0,0);
+    SetConsoleOutputCP(65001);
     int level = 1, life = 3, score = 0, timeremaining = 60, turnsleft = 20;
+    bool gameover = false;
     int squareleft = 50, triangleleft = 50 , circleleft = 50;
     char map[xmax][ymax];
-    clrscr();
-    srand((unsigned)time(NULL));
-    show_cursor();
-    printf("Welcome to ECE Heroes!\n\n");
+    srand(time(NULL));
+    menu();
+    leveload();
     generate_map(map);
-    print_map(map, level, life, score, timeremaining, turnsleft, squareleft, triangleleft, circleleft);
+    gameover = print_map(map, level, life, score, timeremaining, turnsleft, squareleft, triangleleft, circleleft);
+    endscreen(gameover,level);
     return 0;
 }
