fa9747ef4d902ee2f1934da82ff86669dd31a787
agathecdx <agathe.cadoux@edu.ece.fr>
2025-12-18 16:46:25 +0100

    Update main function and switch case logic
    Refactored switch case for game start and reset stats.

--- Game/main.c
+++ Game/main.c
@@ -5,19 +5,33 @@
 
 
 
-int main() { // je sais pas si c'est necessaire de retoucher ceci, probablement oui
+int main() { // je sais pas si c'est necessaire de retoucher ceci, probablement oui -> j'ai retouché un peu le switch case 3 sinon c bon je crois
+    
     clrscr();
     SetConsoleOutputCP(65001); // UTF-8
+    
     int level = 0, life = 3, score = 0, timeremaining = 60, turnsleft = 20 ,squareleft = 50, triangleleft = 50 , circleleft = 50;
     int stats[8] = {level, life, score, timeremaining, turnsleft, squareleft, triangleleft, circleleft};
     bool gameover = false;
+   
     char map[ymax][xmax];
     int i;
+    
     srand(time(NULL));
+    
     do{
         i = menu();
    switch (i) {
-        case 3:
+        case 3: // Start game
+            // réinitialisation des stats pour une nouvelle partie
+            stats[0] = 0;   // level
+            stats[1] = 3;   // life
+            stats[2] = 0;   // score
+            stats[3] = 60;  // timeremaining
+            stats[4] = 20;  // turnsleft
+            stats[5] = 50;  // squareleft
+            stats[6] = 50;  // triangleleft
+            stats[7] = 50;  // circleleft
             playgame(stats, map);
             break;
         case 4:
