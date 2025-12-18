c2158a495a5efc49c1d6bcb07917f09b249e8e7e
agathecdx <agathe.cadoux@edu.ece.fr>
2025-12-18 14:26:19 +0100

    Implement save and load progress features
    Added functionality to save and load player progress with error handling.

--- Game/fichiers.c
+++ Game/fichiers.c
@@ -1,5 +1,5 @@
 #include "Fichiers.h"
-
+#define SAVEFILE "savegame.txt"
 
 
 void saveprogress(int level) {
@@ -7,15 +7,58 @@ void saveprogress(int level) {
     set_color(WHITE, BLACK);
     printf("Enter your name to save progress: ");
     scanf("%49s", playername);
+
+    FILE *f = fopen(SAVEFILE, "a"); // ouvre le fichier pour écrire/ajouter, sans écraser
+    if (f == NULL) {
+        printf("Error: cannot open save file.\n");
+        return;
+    }
+
+    // On sauvegarde : pseudo + toutes les stats
+    fprintf(f, "%s %d %d %d %d %d %d %d %d\n",
+            playername,
+            stats[0], stats[1], stats[2], stats[3], stats[4],
+            stats[5], stats[6], stats[7]);
+    fclose(f);
+
+    printf("Progress saved for player '%s'!\n", playername);
+
 }
 
 
-
+// charge progression d’un joueur
 void leveload(void) {
-    char playername[50];
+    char playername[50], , line[200], name_in_file[50];
+    bool found = false;
+    
     set_color(WHITE, BLACK);
     printf("Enter your name to load progress: ");
     scanf("%49s", playername);
-    printf("welcome back john doe! loading level 2...\n");
-    printf("\n name not found, try again\n");
+    FILE *f = fopen(SAVEFILE, "r");
+    if (f == NULL) {
+        printf("No save file found.\n");
+        return;
+    }
+
+    // Parcours du fichier ligne par ligne
+    while (fgets(line, sizeof(line), f)) {
+        sscanf(line, "%s %d %d %d %d %d %d %d %d",
+               name_in_file,
+               &stats[0], &stats[1], &stats[2], &stats[3], &stats[4],
+               &stats[5], &stats[6], &stats[7]);
+
+        if (strcmp(name_in_file, playername) == 0) {
+            found = true;
+            break;
+        }
+    }
+
+    fclose(f);
+    
+    if (found) {
+        printf("welcome back john doe! loading last level...\n");
+    } else{
+        printf("\n name not found, try again\n");
+    }
+    
 }
